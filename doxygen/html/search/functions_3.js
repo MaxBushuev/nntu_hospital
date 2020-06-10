1adda319368d44de2df0f6ba4c75f8ee33c15b5e
Dima Gusev <62891657+qwertyqwe55@users.noreply.github.com>
2020-06-10 04:02:00 +0300

    Create functions_3.js

---
+++ doxygen/html/search/functions_3.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['getcantake',['getCanTake',['../classhospDepartment.html#ae999d1b061750e3bb693470c54ef1bbb',1,'hospDepartment']]],
+  ['getdate',['getDate',['../classPatient.html#ab48343d713eda36f6734b4d8febb1f63',1,'Patient']]],
+  ['getdays',['getDays',['../classPatient.html#a0e5974429890327975af8c59fe603e9d',1,'Patient']]],
+  ['getdepartment',['getDepartment',['../classPatient.html#ab74f6482e09dd6f9a15007a9eaaa1ac4',1,'Patient']]],
+  ['getdiagnosis',['getDiagnosis',['../classPatient.html#a7ccf1f0ffbb32e812ce1052c0ba80086',1,'Patient']]],
+  ['getdoctors',['getDoctors',['../classhospDepartment.html#a42ba40e105dddb821e05f065924b0074',1,'hospDepartment']]],
+  ['getname',['getName',['../classhospDepartment.html#a8bc42cbd0a3c6e01391249f7b5882c08',1,'hospDepartment::getName()'],['../classPatient.html#a69910a374b455ce5c5f148a93279d9cf',1,'Patient::getName()']]],
+  ['getplacenumber',['getPlaceNumber',['../classhospDepartment.html#a8e62be76907bc62f3393bd2bfc5b0f6f',1,'hospDepartment']]]
+];
