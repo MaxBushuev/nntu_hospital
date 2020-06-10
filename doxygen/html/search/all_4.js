6a2928d10b069751304ac737f6938acd4cb7c6c5
Dima Gusev <62891657+qwertyqwe55@users.noreply.github.com>
2020-06-10 04:00:40 +0300

    Create all_4.js

---
+++ doxygen/html/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hospdepartment',['hospDepartment',['../classhospDepartment.html',1,'hospDepartment'],['../classhospDepartment.html#ae3398a585aa9773fa00486022560af9c',1,'hospDepartment::hospDepartment()']]]
+];
