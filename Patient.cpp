56e6cb4478fea3656751c3c4a8b6ff7992b069ad
Dima Gusev <62891657+qwertyqwe55@users.noreply.github.com>
2020-06-10 03:52:28 +0300

    Update Patient.cpp

--- Patient.cpp
+++ Patient.cpp
@@ -2,6 +2,19 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+/**
+ * @file Patient.txt
+ * @brief Реализация класса Patient
+ *  
+ * @param name ФИО пациента
+ * @param diagnosis Диагноз пациента
+ * @param departament Отделение, в котором лежит пациент
+ * @param days Дней госпитализации
+ * @param date Дата, когда пациент был госпитализирован
+ */
+
+
 Patient::Patient(){
 		_name = "";
 		_diagnosis = "";
@@ -10,6 +23,7 @@ Patient::Patient(){
 		_date = "";
 		_next = NULL;
 }
+
 Patient::Patient(string name, string diagnosis, string department, int days, string date){
 	_name = name;
 	_diagnosis = diagnosis;
@@ -18,24 +32,84 @@ Patient::Patient(string name, string diagnosis, string department, int days, str
 	_date = date;
 	_next = NULL;
 }
+
+/**
+ * @brief Метод, который меняет имя пациента на другое
+ * 
+ * @code
+ * void Patient::setName(string name){
+		_name = name;
+    }
+	@endcode
+ */ 
+
 void Patient::setName(string name){
 	_name = name;
 }
+
+/**
+ * @brief Метод, который меняет диагноз пациента на другой
+ * 
+ * @code
+ *	void Patient::setDiagnosis(string diagnosis){
+		_diagnosis = diagnosis;
+	}
+	@endcode
+ */ 
+
+
 void Patient::setDiagnosis(string diagnosis){
 	_diagnosis = diagnosis;
 }
+
+/**
+ * @brief Метод, который меняет отделение пациента на другое
+ * 
+ * @code
+ 	void Patient::setDepartment(string department){
+		_department = department;
+	}
+	@endcode
+ */ 
+
 void Patient::setDepartment(string department){
 	_department = department;
 }
+
+/**
+ * @brief Метод, который меняет количество дней госпитализации пациента на другое
+ * 
+ * @code
+	void Patient::setDays(int days){
+		_days = days;
+	}
+	@endcode
+ */ 
+
 void Patient::setDays(int days){
 	_days = days;
 }
+
+/**
+ * @brief Метод, который меняет дату, когда пациента госпитализировали на другую
+ * 
+ * @code
+	void Patient::setDate(string date){
+		_date = date;
+	}
+	@endcode
+ */ 
+
 void Patient::setDate(string date){
 	_date = date;
 }
+
+
 void Patient::setNext(Patient* next){
 	_next = next;
 }
+
+
 void Patient::print(){
         cout << "Name: " << _name << endl;
         cout << "diagnosis: " << _diagnosis << endl;
@@ -43,21 +117,48 @@ void Patient::print(){
         cout << "days:" << _days << endl;
         cout << "date: " << _date << endl;
 }
+
+
+
+/**
+ *  @return Имя пациента
+ */ 
 string Patient::getName(){
 	return _name;
 }
+
+/**
+ *  @return Диагноз пациента
+ */ 
+
 string Patient::getDiagnosis(){
 	return _diagnosis;
 }
+
+/**
+ *  @return Отделение, в котором лежит пациент
+ */ 
+
 string Patient::getDepartment(){
 	return _department;
 }
+
+/**
+ *  @return Количество дней госпитализации пациента
+ */ 
+
 int Patient::getDays(){
 	return _days;
 }
+
+/**
+ *  @return Дату госпитализации пациента
+ */ 
+
 string Patient::getDate(){
 	return _date;
 }	
+
 Patient* Patient::next(){
 	return _next;	
 }
