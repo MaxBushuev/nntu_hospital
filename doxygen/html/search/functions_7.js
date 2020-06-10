7973857ddbc06fa84f81f341fe56d183ebd688ba
Dima Gusev <62891657+qwertyqwe55@users.noreply.github.com>
2020-06-10 04:02:19 +0300

    Create functions_7.js

---
+++ doxygen/html/search/functions_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readfromfile',['readFromFile',['../classhospDepartment.html#a6f13da830ee900355da5ba3a2700c89d',1,'hospDepartment']]],
+  ['remove',['remove',['../classpatientList.html#a5ac1c0a8a97d266110f47265be7da3d2',1,'patientList']]]
+];
